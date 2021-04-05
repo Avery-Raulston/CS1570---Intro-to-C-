be1c387db75cd52a4a816891bdf5e22a2e08f03e
user <user@localhost.localdomain>
2021-04-05 11:50:22 -0500

    getting started

---
+++ the_project/main.cpp
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////////////////////////
+//Programmer: Avery Raulston
+//Professor: Price
+//File: main.cpp
+//Description: this file contains the main function for hw7 'the_project'
+//Date Last edited: Apr 5, 2021
+////////////////////////////////////////////////////////////////////////////
+#include<iostream>
+#include "main.h"
+using namespace std;
+
+int main()
+{
+
+  srand(time(NULL));
+  return 0;
+}
---
+++ the_project/main.h
@@ -0,0 +1,12 @@
+///////////////////////////////////////////////////////////
+//Programmer: Avery Raulston
+//Professor: Price
+//File: main.h
+//Description: this file is the header file for main.cpp
+//Date last edited: Apr 5, 2021
+///////////////////////////////////////////////////////////
+
+#ifndef "main.h"
+#define "main.h"
+
+#endif
